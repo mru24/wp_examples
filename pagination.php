0358a597f0b73600aefd56f8f1cdd901bb62defc
mru24 <mru24@yahoo.com>
2020-12-29 09:46:19 +0000

    pagination updated

--- pagination.php
+++ pagination.php
@@ -20,12 +20,19 @@ $loop = new WP_Query( array('post_type' => 'custom_post_type', 'orderby' => 'pos
   <?php
     $big = 999999999;
     echo paginate_links( array(
-      'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
-      'format' => '?paged=%#%',
-      'current' => max( 1, get_query_var('paged') ),
-      'total' => $loop->max_num_pages,
-      'prev_text' => '&laquo;',
-      'next_text' => '&raquo;'
+      'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+      'total'        => $query->max_num_pages,
+      'current'      => max( 1, get_query_var( 'paged' ) ),
+      'format'       => '?paged=%#%',
+      'show_all'     => false,
+      'type'         => 'plain',
+      'end_size'     => 2,
+      'mid_size'     => 1,
+      'prev_next'    => true,
+      'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+      'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+      'add_args'     => false,
+      'add_fragment' => '',
       )
     );
   ?>
