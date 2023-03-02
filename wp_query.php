1d86ac030e6ef2d4921d384641a9ac92be99e3e3
mru24 <mru24@yahoo.com>
2023-03-02 10:07:06 +0000

    pagination added to wp_query

--- wp_query.php
+++ wp_query.php
@@ -1,17 +1,50 @@
-<?php $loop = new WP_Query( array('post_type' => 'acf_post_name', 'orderby' => 'post_id',  'order' => 'DESC', 'posts_per_page' => -1 ) ); ?>
-<?php while( $loop->have_posts() ) :  $loop->the_post(); ?>
 <?php
-  $image = get_field('image');
-  $title = get_field('title');
-  $text = get_field('text');
-  $button = get_field('button');
-  $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id() );
-?>
-	<div class="box">
+	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+	$loop = new WP_Query( array(
+		'post_type' => 'acf_post_name',
+		'orderby' => 'post_id',
+		'order' => 'DESC',
+		'posts_per_page' => -1,
+		'paged' => $paged
+	) );
+	while( $loop->have_posts() ) : $loop->the_post();
 
-		<?php echo $title; ?>
+		$field = get_field('custom_field');
+		$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id() );
+
+?>
+	<div>
+
+		<h1><?php the_title() ?></h1>
+
+		<?php echo get_the_date(); ?>
+
+		<?php the_excerpt(); ?>
+
+		<p><?php the_content(); ?></p>
 
 	</div>
 
+<?php endwhile; ?>
 
-<?php endwhile; wp_reset_query(); ?>
+<div class="pagination">
+    <?php
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $loop->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
+    ?>
+</div>
+
+<?php wp_reset_query();
